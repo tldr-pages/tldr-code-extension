873f9a6d344cef5c5f740f67e4e62edfda7bdad3
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2022-12-15 19:28:15 +1000

    Use classes instead of functions for parser

--- tldr-lint-parser.js
+++ tldr-lint-parser.js
@@ -1,6 +1,52 @@
 const path = require("path");
 const fs = require("fs");
 
+/** Message from linter */
+class Message {
+  #line = 0
+  #id = 0
+  #description = ""
+
+  constructor(line, id, description) {
+    this.#line = line
+    this.#id = id
+    this.#description = description
+  }
+
+  get line() {
+    return this.#line;
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get description() {
+    return this.#description;
+  }
+}
+
+/** Linter output parser */
+class Parser {
+  #input = ""
+
+  constructor(input) {
+    this.#input = input
+  }
+
+  parse() {
+    return this.#input.split(/\r?\n/).map(currentLine => {
+      let items = currentLine.split(":");
+
+      let messageLine = items[1];
+      let messageId = currentLine.match(/TLDR(\d{3})/)[1];
+      let messageDescription = items[2].replace(/\s+TLDR\d{3}\s+/, "");
+
+      return new Message(messageLine, messageId, messageDescription);
+    });
+  }
+}
+
 function getPaths() {
   return [...new Set((process.env.PATH || '').split(path.delimiter))].map(currentPath => {
     currentPath = path.normalize(currentPath);
@@ -28,24 +74,9 @@ function getExecutablePath(executableName) {
   throw new Error(`Several path contain '${executableName}'`);
 }
 
-function parseLinterOutput(output) {
-  let lines = output.split(/\r?\n/);
-  return lines.map(currentLine => {
-    let items = currentLine.split(":");
-    let errorLineNumber = items[1];
-    let errorId = currentLine.match(/TLDR(\d{3})/)[1];
-    let errorDescription = items[2].replace(/\s+TLDR\d{3}\s+/, "");
-
-    return {
-      line: errorLineNumber,
-      id: errorId,
-      description: errorDescription
-    }
-  });
-}
-
 module.exports = {
   isInPath,
   getExecutablePath,
-  parseLinterOutput
+  Message,
+  Parser
 }
